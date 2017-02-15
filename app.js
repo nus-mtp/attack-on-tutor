54fccaa8254b5f99f780c3b462daf692df55f0ea
NUSSTU\A0125566 <lee.w.zheng@gmail.com>
2017-02-15 16:23:26 +0800

    Add listener for app server.

--- app.js
+++ app.js
@@ -26,3 +26,15 @@ app.use
 		next (err);
 	}
 );
+
+var server = app.listen
+(
+	8081,
+	function ()
+	{
+		var host = server.address().address
+		var port = server.address().port
+
+		console.log("App listening at http://%s:%s", host, port)
+	}	
+)
