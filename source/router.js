346f925e7c9489fb40cdaca5bd00863020d7b9bf
yeojoey <yeojoey@gmail.com>
2017-03-18 16:32:31 +0800

    Add api route for lobby authentication

--- source/router.js
+++ source/router.js
@@ -10,7 +10,7 @@ var index = require ('./controller/index');
 
 var lobby = require ('./controller/lobby');
 
-router.get ('/lobby/:userId/:moduleId/:tutorialId', lobby.get);
+router.get ('/lobby/:userId/:moduleId/:tutorialId', auth.ensureAuth, lobby.get);
 
 var login = require ('./controller/login');
 var dashboard = require('./controller/dashboard');
@@ -23,5 +23,7 @@ router.get ('/dashboard', auth.ensureAuth, dashboard.get);
 
 router.post('/api/dashboard/forceSyncIVLE', auth.ensureAuth, dashboard.forceSyncIVLE);
 router.post('/api/dashboard/getTutorials', auth.ensureAuth, dashboard.getTutorials);
+
+router.get('/api/lobby/enterLobby', auth.ensureAuth, lobby.enterLobby);
 
 module.exports = router;
