a9e033e8fd278385b8bf4aa6efdc428b42c5afbf
glutSolidSphere <lee.w.zheng@gmail.com>
2017-04-12 02:28:41 +0800

    Fix error with $, please check and handle this case if it's actually used for anything. Thank you.

--- public/javascripts/dashboard/dashboard.js
+++ public/javascripts/dashboard/dashboard.js
@@ -104,6 +104,7 @@ function toTitleCase(str) {
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
+/*
 var logoutConfirmation = "Would You Like to Log Out?";
 	
 $("#logout").on
@@ -115,3 +116,4 @@ $("#logout").on
 		location.reload();
 	}
 );
+*/
