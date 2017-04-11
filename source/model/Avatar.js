32d1a21932b43e7818b0405b13d0631cd53c300f
yeojoey <yeojoey@gmail.com>
2017-04-11 16:40:50 +0800

    Add avatar model

---
+++ source/model/Avatar.js
@@ -0,0 +1,27 @@
+/**
+ * @module model/Avatar
+ * @type {Sequelize|*|exports|module.exports}
+ */
+var sequelize = require ('../sequelize');
+var Sequelize = require ('sequelize');
+var User = require ('./User');
+
+/**
+ * Define avatar model
+ * @type {Model}
+ */
+var Avatar = sequelize.define('avatar', {
+	id: {
+		type: Sequelize.UUID,
+		defaultValue: Sequelize.UUIDV4,
+		unique: true,
+		primaryKey: true
+	},
+	price: { type: Sequelize.INTEGER },
+	url: { type: Sequelize.STRING }
+});
+
+Avatar.belongsToMany(User, {
+	foreignKey: 'avatarId',
+	through: 'userAvatar'
+});
