2a871772fb078d80de72a312748cd70a4ad2cd30
yeojoey <yeojoey@gmail.com>
2017-04-15 14:48:13 +0800

    Remove reference to updated model

--- source/controller/login.js
+++ source/controller/login.js
@@ -48,8 +48,7 @@ var callback = function (req, res, next) {
 						email: result.Email,
 						gender: result.Gender,
 						token: result.Token,
-						avatarId: 'avatar-01',
-						levelsSpent: 0
+						avatarId: 'avatar-01'
 					}).then(function(user){
 						var authToken = auth.setAuth (result.UserID, result.Name);
 						//logger.info(result.UserID + ' created user');
