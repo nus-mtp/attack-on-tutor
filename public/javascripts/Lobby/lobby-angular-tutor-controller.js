1c3aa2de9cc489f5b6412f5ed3758b0231d98d6e
glutSolidSphere <lee.w.zheng@gmail.com>
2017-04-17 23:31:00 +0800

    Commenting and code clean up.

--- public/javascripts/Lobby/lobby-angular-tutor-controller.js
+++ public/javascripts/Lobby/lobby-angular-tutor-controller.js
@@ -1,3 +1,9 @@
+/**
+ * Controller for the tutor functionalities within the lobby.
+ * Front-end javascript code inside public folder.
+ *
+ * @module javascripts/lobby/lobby-angular-tutor-controller
+ */
 angular.module('lobbyApp').controller ('tutorCtrl', function($scope, socket) {
     $scope.userInfo = {};
 	$scope.socket = socket;
@@ -9,13 +15,18 @@ angular.module('lobbyApp').controller ('tutorCtrl', function($scope, socket) {
     };
     $scope.questions = {};
 
+    /*
+     *   Listeners for tutor client.
+     */
+
+    //Listen for login response from server before initialising everything else.
     socket.on ('login', function (data) {
 
-        console.log ( data );
-
         $scope.userInfo.imgSrc = data.userAvatar;
-
+        //Ensure the user logged in is a tutor, otherwise do not initialise all these socket listeners.
         if (data.userType == 'tutor') {
+
+            //Receive the answers submitted by student clients for grading from the server.
             socket.on ('submit answer', function (data) {
                 var question = $scope.questions[data.uuid];
                 if (question) {
@@ -28,9 +39,11 @@ angular.module('lobbyApp').controller ('tutorCtrl', function($scope, socket) {
                 }
             });
 
+            //Log the question in this client after it has been sent out to all the students in the selected groups by the server.
             socket.on ('log question', function (data) {
                 //Object to store the answers receieved from students later.
                 var groupAnswers = {};
+                //Each group has its own answer.
                 data.question.groups.forEach (function (value) {
                     groupAnswers[value] = {
                         'student' : '',
@@ -57,19 +70,36 @@ angular.module('lobbyApp').controller ('tutorCtrl', function($scope, socket) {
         }
     });
 
+    /*
+     *  Scope functions used by angular in the DOM.
+     */
+
+    /**
+     * Check if the index of the current group is already selected in the question composer.
+     *
+     * @param {Integer} index
+     * @returns {Boolean}
+     */
     $scope.inSelectedGroups = function (index) {
 		return ($scope.selectedGroups.indexOf (index) > -1);
 	};
 
+    /**
+     * Select/deselect a group to send the question to in the question composer.
+     *
+     * @param {Integer} index
+     */
 	$scope.toggleSelectedGroup = function (index) {
 		if ($scope.inSelectedGroups(index)) {
 			delete $scope.selectedGroups[$scope.selectedGroups.indexOf (index)];
 		} else {
+            //The first group is the default group for the entire lobby.
             if (index == 0) {
                 $scope.selectedGroups = [];
             }
             else
             {
+                //When any sub groups are selected prevent the tutor from sending to the group that contains the entire tutorial as well.
                 if ($scope.inSelectedGroups(0)) {
                     delete $scope.selectedGroups[$scope.selectedGroups.indexOf (0)];
                 }
@@ -78,6 +108,12 @@ angular.module('lobbyApp').controller ('tutorCtrl', function($scope, socket) {
 		}
 	};
 
+    /**
+     * Select the group for viewing in the tutor's grading interface.
+     *
+     * @param {String} uuid
+     * @param {Integer} index
+     */
     $scope.setSelectedGroup = function (uuid, index) {
         var question = $scope.questions[uuid];
 
@@ -86,8 +122,12 @@ angular.module('lobbyApp').controller ('tutorCtrl', function($scope, socket) {
         }
     };
 
+    /**
+     * Send the composed question to the server.
+     */
 	$scope.sendQuestion = function () {
 		var groupNames = [];
+        //Get the names of all the selected groups.
 		$scope.selectedGroups.forEach (function (value) {
             if ($scope.socket.getSocketGroups()[value]) {
                 groupNames.push ($scope.socket.getSocketGroups()[value]);
@@ -104,6 +144,12 @@ angular.module('lobbyApp').controller ('tutorCtrl', function($scope, socket) {
 		$scope.selectedGroups = [];
 	};
 
+    /**
+     * Check if a question has been answered by the group.
+     *
+     * @param {String} uuid
+     * @returns {Boolean}
+     */
     $scope.isQuestionAnswered = function (uuid) {
         var question = $scope.questions[uuid];
 
@@ -117,13 +163,20 @@ angular.module('lobbyApp').controller ('tutorCtrl', function($scope, socket) {
         return true;
     };
 
+    /**
+     * Send the tutor's explanation and grade back to the server.
+     *
+     * @param {String} uuid
+     * @param {Integer} index
+     */
     $scope.gradeQuestion = function (uuid) {
         var question = $scope.questions[uuid];
         question.graded = true;
 
-        healthLeft = $scope.health;
+        var healthLeft = $scope.health;
         for (var group in question.groupAnswers) {
             if (question.groupAnswers.hasOwnProperty (group)) {
+                //Deduct the experience given to the group from the tutor's health and let the server know.
                 healthLeft = $scope.health - question.groupAnswers[group].experience;
                 socket.emit ('damage shoutout', {
                     'group' : group,
@@ -132,6 +185,7 @@ angular.module('lobbyApp').controller ('tutorCtrl', function($scope, socket) {
             }
         }
 
+        //Let the server know to grant all students in the lobby the experience payout when the tutor's health is below 0.
         if (healthLeft > 0) {
             $scope.health = healthLeft;
         } else {
