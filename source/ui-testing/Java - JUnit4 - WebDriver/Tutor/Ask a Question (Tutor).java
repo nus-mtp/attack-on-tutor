4cdebdbc62232af5affd9d401c5d96493373248a
DESKTOP-CO22UMA\silfe <silfer.taintedcross@outlook.com>
2017-04-18 23:23:53 +0800

    UI Tests

---
+++ source/ui-testing/Java - JUnit4 - WebDriver/Tutor/Ask a Question (Tutor).java
@@ -0,0 +1,74 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AskAQuestionTutor {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://127.0.0.1:8081/dashboard";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testAskAQuestionTutor() throws Exception {
+    driver.findElement(By.xpath("//textarea[@type='text']")).clear();
+    driver.findElement(By.xpath("//textarea[@type='text']")).sendKeys("123");
+    driver.findElement(By.xpath("//div[@id='question-list-container']/div[3]/div/div/div/button")).click();
+    driver.findElement(By.xpath("//div[@id='question-list-container']/div[3]/div/div/div/button[2]")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
